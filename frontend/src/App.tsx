929176fba4f9820447dba6c1188ae19bbb904414
Caroline Araujo <ca.araujosouza@gmail.com>
2021-05-04 22:41:09 -0300

    erro no donut char

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChar from "components/BarChar";
 import DataTable from "components/dataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/footer";
 import Navbar from "components/navbar";
 
@@ -7,11 +9,25 @@ function App() {
     <>
       <Navbar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-        <DataTable />
-      </div>
-      <Footer />
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
+            <BarChar />
+          </div>
+            <div className="col-sm-6">
+              <h5 className="text-center text-secundary" >Todas as Vendas</h5>
+              <DonutChart />
+            </div>
+          </div>
+
+          <div className="py-3">
+            <h2 className="text-primary">Todas vendas</h2>
+          </div>
+          <DataTable />
+        </div>
+        <Footer />
     </>
   );
 }
