724cd2f49984fc6148076a51e18979f511dd9398
Caroline Araujo <ca.araujosouza@gmail.com>
2021-05-09 19:34:55 -0300

    Group By Search

--- backend/src/main/java/com/devsuperior/dsvendas/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controller/SaleController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
-import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.services.SaleService;
-import com.devsuperior.dsvendas.services.SellerService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -25,6 +25,18 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
 		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupBySeller(){
+		List<SaleSumDTO> list = service.amountGroupBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupBySeller(){
+		List<SaleSuccessDTO> list = service.successGroupBySeller();
 		return ResponseEntity.ok(list);
 	}
 }
