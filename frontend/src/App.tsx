58d934f8d61e9f80d0bce84ad059b2fea23e7ed1
Caroline Araujo <ca.araujosouza@gmail.com>
2021-05-10 20:25:50 -0300

    Add Routes React

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,8 @@
-import BarChar from "components/BarChar";
-import DataTable from "components/dataTable";
-import DonutChart from "components/DonutChart";
-import Footer from "components/footer";
-import Navbar from "components/navbar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secundary">Taxa de Sucesso</h5>
-            <BarChar />
-          </div>
-            <div className="col-sm-6">
-              <h5 className="text-center text-secundary" >Todas as Vendas</h5>
-              <DonutChart />
-            </div>
-          </div>
-
-          <div className="py-3">
-            <h2 className="text-primary">Todas vendas</h2>
-          </div>
-          <DataTable />
-        </div>
-        <Footer />
-    </>
+    <Routes />
   );
 }
 
